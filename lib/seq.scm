d364aad655aed0327067a16654a4ad0bad6cd761
Jason Felice <jason.m.felice@gmail.com>
2013-04-20 22:15:53 -0400

    Make strings seqable

--- lib/seq.scm
+++ lib/seq.scm
@@ -13,6 +13,20 @@
 (add-method (end? (list? object))
   (null? object))
 
+(define-type string-seq
+  (string read-only:)
+  (offset read-only:))
+
+(add-method (seq (string? object))
+  (make-string-seq object 0))
+(add-method (first (string-seq? object))
+  (string-ref (string-seq-string object) (string-seq-offset object)))
+(add-method (rest (string-seq? object))
+  (make-string-seq (string-seq-string object) (+ 1 (string-seq-offset object))))
+(add-method (end? (string-seq? object))
+  (= (string-length (string-seq-string object))
+     (string-seq-offset object)))
+
 (define (for-each proc seqable)
   (let loop ((iterator (seq seqable)))
     (if (end? iterator)
