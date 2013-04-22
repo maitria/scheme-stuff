fa2c0c455b7c4a8c7e08350a94527ce028665b7c
Jason Felice <jason.m.felice@gmail.com>
2013-04-22 10:32:38 -0400

    Remove some unneccesary BEGINs

--- lib/generics#.scm
+++ lib/generics#.scm
@@ -4,23 +4,20 @@
   ))
 
 (define-macro (define-generic form)
-  (let ((generic-name (car form)))
-    `(begin
-       (define ,generic-name
-         (let (($methods (box '())))
-	   (lambda args
-	     (dispatch-generic (unbox $methods) args)))))))
+  `(define ,(car form)
+     (let (($methods (box '())))
+       (lambda args
+	 (dispatch-generic (unbox $methods) args)))))
 
 (define-macro (add-method form . body)
   (let* ((generic-name (car form))
 	 (args (cdr form))
 	 (arg-names (map cadr args)))
-    `(begin
-       (let (($methods (generic-methods-box ,generic-name)))
-	 (set-box! $methods (cons
-			      (cons
-				(lambda ,arg-names
-				  (and ,@args))
-				(lambda ,arg-names
-				  ,@body))
-			      (unbox $methods)))))))
+    `(let (($methods (generic-methods-box ,generic-name)))
+       (set-box! $methods (cons
+			    (cons
+			      (lambda ,arg-names
+				(and ,@args))
+			      (lambda ,arg-names
+				,@body))
+			    (unbox $methods))))))
