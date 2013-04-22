bb41284a22069e279a6bd6f3ce805bf9e7c4d261
Jason Felice <jason.m.felice@gmail.com>
2013-04-22 12:31:07 -0400

    Allow methods to have unqualified args

--- lib/generics#.scm
+++ lib/generics#.scm
@@ -12,12 +12,29 @@
 (define-macro (add-method form . body)
   (let* ((generic-name (car form))
 	 (args (cdr form))
-	 (arg-names (map cadr args)))
+	 (arg-name (lambda (arg)
+		     (if (pair? arg)
+		       (cadr arg)
+		       arg)))
+	 (arg-names (map arg-name args))
+	 (arg-tests (let loop ((args-remaining args)
+			       (tests-found '()))
+		      (cond
+			((null? args-remaining)
+			 (reverse tests-found))
+			((pair? (car args-remaining))
+			 (loop 
+			   (cdr args-remaining)
+			   (cons (car args-remaining) tests-found)))
+			(else
+			 (loop
+			   (cdr args-remaining)
+			   tests-found))))))
     `(let (($methods (generic-methods-box ,generic-name)))
        (set-box! $methods (cons
 			    (cons
 			      (lambda ,arg-names
-				(and ,@args))
+				(and ,@arg-tests))
 			      (lambda ,arg-names
 				,@body))
 			    (unbox $methods))))))
