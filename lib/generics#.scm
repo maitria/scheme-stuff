7ed5daa292708d53c24362f62c3d15b9d3bb41f2
Jason Felice <jason.m.felice@gmail.com>
2013-04-22 10:18:59 -0400

    Simplify generics using BOX and ##CLOSURE-REF

--- lib/generics#.scm
+++ lib/generics#.scm
@@ -3,33 +3,29 @@
   ))
 
 (define-macro (define-generic form)
-  (let* ((generic-name (car form))
-	 (method-table-name (string->symbol
-			      (string-append
-				"*"
-				(symbol->string generic-name)
-				"-methods*"))))
-  `(begin
-     (define ,method-table-name '())
-     (define (,generic-name . args)
-       (dispatch-generic ,method-table-name args)))))
+  (let ((generic-name (car form)))
+    `(begin
+       (define ,generic-name
+         (let (($methods (box '())))
+	   (lambda args
+	     (dispatch-generic (unbox $methods) args)))))))
 
 (define-macro (add-method form . body)
   (let* ((generic-name (car form))
-	 (method-table-name (string->symbol
-			      (string-append
-				"*"
-				(symbol->string generic-name)
-				"-methods*")))
 	 (args (cdr form))
-	 (arg-names (map cadr args))
-	 (arg-types (map car args)))
+	 (arg-names (map cadr args)))
     `(begin
-       (set! ,method-table-name (cons
-				  (cons
-				    (lambda ,arg-names
-				      (and ,@args))
-				    (lambda ,arg-names
-				      ,@body))
-				  ,method-table-name))
-       )))
+       (let (($methods (cond
+			 ((##interp-procedure? ,generic-name)
+			  (##vector-ref (##interp-procedure-rte ,generic-name) 1))
+			 ((##closure? ,generic-name)
+			  (##closure-ref ,generic-name 1))
+			 (else
+			  (error "ADD-METHOD called on non-generic")))))
+	 (set-box! $methods (cons
+			      (cons
+				(lambda ,arg-names
+				  (and ,@args))
+				(lambda ,arg-names
+				  ,@body))
+			      (unbox $methods)))))))
