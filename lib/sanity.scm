12fbc55510ae19b5bcee4d7e1109c3a6e234fb6a
Jason Felice <jason.m.felice@gmail.com>
2013-04-22 12:43:22 -0400

    Generic LENGTH, NTH, SET-NTH!

--- lib/sanity.scm
+++ lib/sanity.scm
@@ -1,12 +1,35 @@
+(include "generics#.scm")
 
-(let ((original-length length))
-  (set! length
-    (lambda (o)
-      (cond
-	((string? o)
-	 (string-length o))
-	((vector? o)
-	 (vector-length o))
-	(else
-	 (original-length o))))))
+(define-generic (length thing))
+(define-generic (nth index thing))
+(define-generic (set-nth! index thing value))
 
+(add-method (length (list? l))
+  (##length l))
+(add-method (nth index (list? l))
+  (list-ref l index))
+(add-method (set-nth! index (list? l) value)
+  (let loop ((i index)
+	     (l l))
+    (cond
+      ((= 0 i)
+       (set-car! l value))
+      ((< i 0)
+       (error "SET-NTH! argument must be non-negative"))
+      (else
+       (loop (- i 1) (cdr l)))))
+  #!void)
+
+(add-method (length (vector? v))
+  (##vector-length v))
+(add-method (nth index (vector? v))
+  (vector-ref v index))
+(add-method (set-nth! index (vector? v) value)
+  (vector-set! v index value))
+
+(add-method (length (string? s))
+  (##string-length s))
+(add-method (nth index (string? s))
+  (string-ref s index))
+(add-method (set-nth! index (string? s) value)
+  (string-set! s index value))
