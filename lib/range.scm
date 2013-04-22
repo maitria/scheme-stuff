e2804b6b2000fc856714b52676f816120464bdee
Jason Felice <jason.m.felice@gmail.com>
2013-04-22 11:18:16 -0400

    Make RANGE lazy

--- lib/range.scm
+++ lib/range.scm
@@ -1,10 +1,23 @@
+(include "iteration#.scm")
 
-(define (range #!key (from 0) to (step 1))
-  (define result '())
-  (let loop ((i from))
-    (if (> i to)
-      (reverse result)
-      (begin
-        (set! result (cons i result))
-        (loop (+ i step))))))
+(define-type range-position
+  (value read-only:)
+  (limit read-only:)
+  (step read-only:))
 
+(define (range #!key (from 0) (to #f) (step 1))
+  (make-range-position from to step))
+
+(add-method (first-position (range-position? p))
+  p)
+(add-method (value-at-position (range-position? p))
+  (range-position-value p))
+(add-method (next-position (range-position? p))
+  (make-range-position
+    (+ (range-position-value p) (range-position-step p))
+    (range-position-limit p)
+    (range-position-step p)))
+(add-method (end-position? (range-position? p))
+  (and (range-position-limit p)
+       (> (range-position-value p)
+	  (range-position-limit p))))
