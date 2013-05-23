6d8dfaceebc97c5e080aa88bd4e42b55b443ada6
Jason Felice <jason.m.felice@gmail.com>
2013-05-22 20:50:06 -0400

    Extract READ-ALL-AS-STRING

--- lib/files.scm
+++ lib/files.scm
@@ -1,19 +1,20 @@
+(define (read-all-as-string)
+  (define s (make-string 4096))
+  (let loop ((c (read-char))
+	     (i 0))
+    (cond
+      ((eof-object? c)
+       (substring s 0 i))
+      (else
+       (if (= i (string-length s))
+	 (let ((ns (make-string (* 2 (string-length s)))))
+	   (substring-move! s 0 i ns 0)
+	   (set! s ns)))
+       (string-set! s i c)
+       (loop (read-char) (+ i 1))))))
+
 (define (read-file f)
-  (with-input-from-file f
-    (lambda ()
-      (define s (make-string 4096))
-      (let loop ((c (read-char))
-	         (i 0))
-	(cond
-	  ((eof-object? c)
-	   (substring s 0 i))
-	  (else
-	   (if (= i (string-length s))
-	     (let ((ns (make-string (* 2 (string-length s)))))
-	       (substring-move! s 0 i ns 0)
-	       (set! s ns)))
-	   (string-set! s i c)
-	   (loop (read-char) (+ i 1))))))))
+  (with-input-from-file f read-all-as-string))
 
 (add-method (->list (input-port? port))
   (producer->position
